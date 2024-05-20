ee472490acd77f61a2e87ee4aa73862f556c8406
Matias Moreno Iglesias <moreno.iglesias07@gmail.com>
2024-05-20 17:10:16 -0400

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/matiasmorenoiglesias/aprendiendo-go
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	//"fmt"
+	"github.com/matiasmorenoiglesias/aprendiendo-go/variables"
+)
+
+func main(){
+	variables.ShowIntegers()
+	// fmt.Println("Hello World");
+}
