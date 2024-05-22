d4d12919ac8da5c4335f8e5e120a4f3c8a22221a
Matias Moreno Iglesias <moreno.iglesias07@gmail.com>
2024-05-22 17:35:41 -0400

    Add: channels and defer and async calls

--- main.go
+++ main.go
@@ -7,14 +7,18 @@ import (
 	//"github.com/matiasmorenoiglesias/aprendiendo-go/files"
 	//"github.com/matiasmorenoiglesias/aprendiendo-go/arrays_slices"
 	//"github.com/matiasmorenoiglesias/aprendiendo-go/mapas"
-	"github.com/matiasmorenoiglesias/aprendiendo-go/defers"
+	//"github.com/matiasmorenoiglesias/aprendiendo-go/defers"
+	"github.com/matiasmorenoiglesias/aprendiendo-go/goroutines"
 	//"github.com/matiasmorenoiglesias/aprendiendo-go/interfaces"
 	//"github.com/matiasmorenoiglesias/aprendiendo-go/users"
 	//"github.com/matiasmorenoiglesias/aprendiendo-go/funciones"
 )
 
-
-func main(){
-	defers.VemosDefer()
-	defers.EjemploPanic()
+func main() {
+	chan1 := make(chan bool)
+	go goroutines.MiNombreLento("Matias", chan1)
+	defer func(){
+		<- chan1
+	}()
+	println("Estoy Aqui!")
 }
